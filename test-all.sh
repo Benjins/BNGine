a8f9692e3fc343812af6463b5d10e000db6cb43c
Benji Smith <benjsith@gmail.com>
2016-04-17 00:18:28 -0400

    More test stuff.

--- test-all.sh
+++ test-all.sh
@@ -1,4 +1,10 @@
+set -e
+
 make -B BNGine.out
 make -B OS_MESA=1 BNGine.out
 valgrind --quiet --leak-check=full --error-exitcode=12 ./BNGine.out
+
+echo "=============================================="
+
+cppcheck --force --inline-suppr --enable=all src/*/*.cpp --quiet --verbose --template '{file},{line},{severity},{id},{message}' --suppress=unusedFunction --suppress=purgedConfiguration --suppress=noExplicitConstructor --suppress=unusedStructMember --suppress=missingIncludeSystem --suppress=cstyleCast
 
