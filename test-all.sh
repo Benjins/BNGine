f9399a3024192ea769f96ff71c92424cfbdf3791
Benji Smith <benjsith@gmail.com>
2016-04-17 08:25:40 -0400

    valgrind was quiet...tooquiet..

--- test-all.sh
+++ test-all.sh
@@ -6,5 +6,5 @@ valgrind --quiet --leak-check=full --error-exitcode=12 ./BNGine.out
 
 echo "=============================================="
 
-cppcheck --force --inline-suppr --enable=all src/*/*.cpp --quiet --verbose --template '{file},{line},{severity},{id},{message}' --suppress=unusedFunction --suppress=purgedConfiguration --suppress=noExplicitConstructor --suppress=unusedStructMember --suppress=missingIncludeSystem --suppress=cstyleCast
+cppcheck --force --inline-suppr --enable=all src/*/*.cpp --verbose --template '{file},{line},{severity},{id},{message}' --suppress=unusedFunction --suppress=purgedConfiguration --suppress=noExplicitConstructor --suppress=unusedStructMember --suppress=missingIncludeSystem --suppress=cstyleCast
 
