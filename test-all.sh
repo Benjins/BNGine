eea6149d64be6a78594ae7fa0c6ba265dd4c9035
Benji Smith <benjsith@gmail.com>
2016-04-17 08:34:14 -0400

    Removing tests for now.

--- test-all.sh
+++ test-all.sh
@@ -2,9 +2,9 @@ set -e
 
 make -B BNGine.out
 make -B OS_MESA=1 BNGine.out
-valgrind --leak-check=full --error-exitcode=12 ./BNGine.out
+#valgrind --leak-check=full --error-exitcode=12 ./BNGine.out
 
-echo "=============================================="
+#echo "=============================================="
 
-cppcheck --force --inline-suppr --enable=all src/*/*.cpp --quiet --verbose --template '{file},{line},{severity},{id},{message}' --suppress=unusedFunction --suppress=purgedConfiguration --suppress=noExplicitConstructor --suppress=unusedStructMember --suppress=missingIncludeSystem --suppress=cstyleCast
+# cppcheck --force --inline-suppr --enable=all src/*/*.cpp --quiet --verbose --template '{file},{line},{severity},{id},{message}' --suppress=unusedFunction --suppress=purgedConfiguration --suppress=noExplicitConstructor --suppress=unusedStructMember --suppress=missingIncludeSystem --suppress=cstyleCast
 
