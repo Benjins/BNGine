0b15edb77f8c42c0b1a63ca46c102aa2213f68f1
Benji Smith <benjsith@gmail.com>
2016-10-21 01:09:11 -0400

    Ha ha, of course I change it to the default.

--- test-all.sh
+++ test-all.sh
@@ -4,7 +4,7 @@ make genCode
 make -B BNGine.out
 make -B OS_MESA=1 BNGine.out
 ./BNGine.out --compare
-valgrind --num-callers=12 --leak-check=full --error-exitcode=12 ./BNGine.out
+valgrind --num-callers=15 --leak-check=full --error-exitcode=12 ./BNGine.out
 
 #cppcheck --force --inline-suppr --enable=all src/*/*.cpp --quiet --verbose --template '{file},{line},{severity},{id},{message}' --suppress=unusedFunction --suppress=purgedConfiguration --suppress=noExplicitConstructor --suppress=unusedStructMember --suppress=missingIncludeSystem --suppress=cstyleCast
 
