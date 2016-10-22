c8bb0fbeee827c57ead2e654139aa47983eb3265
Benji Smith <benjsith@gmail.com>
2016-10-22 13:48:33 -0400

    I am at a loss for words.  If this doesn't work, then the issue is somewhere outside the actual text of the shader?

--- assets/shaders/posCol.fs
+++ assets/shaders/posCol.fs
@@ -1,11 +1,7 @@
 #version 120
 
-//varying vec2 outUv;
-
-//uniform sampler2D _mainTex;
-
-uniform vec4 color;
+uniform vec4 _col;
 
 void main(){
-	gl_FragColor = color;// texture2D(_mainTex, vec2(0.2, 0.2));
+	gl_FragColor = _col;
 }
