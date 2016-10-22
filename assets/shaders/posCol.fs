fccb7cdd999e0898a394acfe8449896606e08c60
Benji Smith <benjsith@gmail.com>
2016-10-22 13:45:02 -0400

    Okay, I'm at a loss for words if this doesn't work.

--- assets/shaders/posCol.fs
+++ assets/shaders/posCol.fs
@@ -2,8 +2,10 @@
 
 //varying vec2 outUv;
 
-uniform sampler2D _mainTex;
+//uniform sampler2D _mainTex;
+
+uniform vec4 color;
 
 void main(){
-	gl_FragColor = texture2D(_mainTex, vec2(0.2, 0.2));
+	gl_FragColor = color;// texture2D(_mainTex, vec2(0.2, 0.2));
 }
