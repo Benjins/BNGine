f3767b8efacc019369f85b4a1bddf4ba4c42ac8b
Benji Smith <benjsith@gmail.com>
2016-10-22 21:18:47 -0400

    Ha ha, let's try that again.

--- test-all.sh
+++ test-all.sh
@@ -1,8 +1,8 @@
 set -e
 
-#make genCode
-#make -B BNGine.out
-#make -B OS_MESA=1 BNGine.out
+make genCode
+make -B BNGine.out
+make -B OS_MESA=1 BNGine.out
 #./BNGine.out --compare
 valgrind --suppressions=valgrind-suppressions.txt --num-callers=15 --leak-check=full --error-exitcode=12 ./BNGine.out
 
