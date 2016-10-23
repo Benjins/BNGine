5b730337be889252f819e0192dad0569a3beec57
Benji Smith <benjsith@gmail.com>
2016-10-22 21:17:03 -0400

    Okay, just suppress the valgrind noise.

--- test-all.sh
+++ test-all.sh
@@ -1,10 +1,10 @@
 set -e
 
-make genCode
-make -B BNGine.out
-make -B OS_MESA=1 BNGine.out
+#make genCode
+#make -B BNGine.out
+#make -B OS_MESA=1 BNGine.out
 #./BNGine.out --compare
-valgrind --gen-suppressions=all --num-callers=15 --leak-check=full --error-exitcode=12 ./BNGine.out
+valgrind --suppressions=valgrind-suppressions.txt --num-callers=15 --leak-check=full --error-exitcode=12 ./BNGine.out
 
 #cppcheck --force --inline-suppr --enable=all src/*/*.cpp --quiet --verbose --template '{file},{line},{severity},{id},{message}' --suppress=unusedFunction --suppress=purgedConfiguration --suppress=noExplicitConstructor --suppress=unusedStructMember --suppress=missingIncludeSystem --suppress=cstyleCast
 
