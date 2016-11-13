29270f7c1c91eed6d39d3c0656a9135ca9ee5301
Benji Smith <benjsith@gmail.com>
2016-11-13 14:57:46 -0500

    Ha ha nvm.

--- test-all.sh
+++ test-all.sh
@@ -6,13 +6,13 @@ if [ -z "$CXX" ]; then
 fi
 
 
-eval "$CXX  -std=c++11 src/unity/UnityBuildGenerator.cpp -o unityBuild.out"
+eval "$CXX -std=c++11 src/unity/UnityBuildGenerator.cpp -o unityBuild.out"
 
 echo "Built unity build generator."
 
 ./unityBuild.out console metagen
 
-eval "$CXX -std=c++11 -Og -g -Wall -DBNS_DEBUG gen/UnityBuild.cpp -o BNSMetaGen.out"
+eval "$CXX -std=c++11 -O0 -g -Wall -DBNS_DEBUG gen/UnityBuild.cpp -o BNSMetaGen.out"
 
 echo "Built metaGen parser."
 
