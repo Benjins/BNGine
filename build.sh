fba29f76d8f122309532e032e476045fcc6ff723
Benji Smith <benjsith@gmail.com>
2016-11-28 13:29:12 -0500

    Build script changes.

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
+set -e
 g++ -std=c++11 src/unity/UnityBuildGenerator.cpp -o unityBuild.out
 
 echo "Built unity build generator."
 
 ./unityBuild.out console metagen
 
-g++ -std=c++11  -Og -g -Wall -DBNS_DEBUG gen/UnityBuild.cpp -o BNSMetaGen.out
+g++ -std=c++11  -O0 -g -Wall -DBNS_DEBUG gen/UnityBuild.cpp -o BNSMetaGen.out
 
 echo "Built metaGen parser."
 
--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,7 @@
 #This performs a quick rebuild of game code.  Does not build unity build generator, or build or run meta code generator.
 
+set -e
+
 ./unityBuild.out x11 $1
 
-g++ -std=c++11  -Og -g -Wall -DBNS_DEBUG gen/*.cpp -lX11 -lGL -o BNgine_$1.out
+g++ -std=c++11  -O0 -g -Wall -DBNS_DEBUG gen/*.cpp -lX11 -lGL -o BNgine_$1.out
