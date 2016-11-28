5e559d1dd986d6c8a567cf724af441036780a2e7
Benji Smith <benjsith@gmail.com>
2016-11-28 16:52:25 -0500

    More build changes.

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 set -e
-g++ -std=c++11 src/unity/UnityBuildGenerator.cpp -o unityBuild.out
+
+if [ -z "$CXX" ]; then
+	echo "Could not find CXX variable, using g++ as default."
+	CXX="g++"
+fi
+
+
+eval "$CXX -std=c++11 src/unity/UnityBuildGenerator.cpp -o unityBuild.out"
 
 echo "Built unity build generator."
 
 ./unityBuild.out console metagen
 
-g++ -std=c++11  -O0 -g -Wall -DBNS_DEBUG gen/UnityBuild.cpp -o BNSMetaGen.out
+eval "$CXX -std=c++11  -O0 -g -Wall -DBNS_DEBUG gen/UnityBuild.cpp -o BNSMetaGen.out"
 
 echo "Built metaGen parser."
 
--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ -z "$CXX" ]; then
+	echo "Could not find CXX variable, using g++ as default."
+	CXX="g++"
+fi
+
 ./unityBuild.out x11 $1
 
-g++ -std=c++11  -O0 -g -Wall -DBNS_DEBUG gen/*.cpp -lX11 -lGL -o BNgine_$1.out
+eval "$CXX -std=c++11  -O0 -g -Wall -DBNS_DEBUG gen/*.cpp -lX11 -lGL -o BNgine_$1.out"
